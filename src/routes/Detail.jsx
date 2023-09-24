41a119d96149c9246c5d49cad81add969d22deea
365saiom <saiom365@naver.com>
2023-09-24 21:41:59 +0900

    detail 페이지 수정

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -3,12 +3,14 @@ import { useParams } from "react-router-dom";
 
 export default function Detail() {
   const [selMovie, setSelMovie] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
+    setLoading(true);
     setSelMovie(json.data.movie);
   };
   useEffect(() => {
@@ -16,8 +18,16 @@ export default function Detail() {
   }, []);
   return (
     <div>
-      <h1>{selMovie.title}</h1>
-      <img src={selMovie.medium_cover_image} />
+      <h1>
+        {loading ? (
+          <h1>선택하신 영화를 조회 중입니다...</h1>
+        ) : (
+          <div>
+            <h1>{selMovie.title}</h1>
+            <img src={selMovie.medium_cover_image} />
+          </div>
+        )}
+      </h1>
     </div>
   );
 }
